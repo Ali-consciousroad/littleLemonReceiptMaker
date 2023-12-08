73377198b5b8e61d80b577f8e550f5754d7cab38
Ali Dindar <ali_dindar@live.be>
2023-12-08 03:06:20 +0100

    Little Lemon Receipt Maker completed

--- finalProject.js
+++ finalProject.js
@@ -1,3 +1,4 @@
+
 // Given variables
 const dishData = [
     {
@@ -20,13 +21,40 @@ const dishData = [
 const tax = 1.20;
 
 // Implement getPrices()
-function getPrices() {
-    
+function getPrices(taxBoolean) {
+    for(i = 0; i < dishData.length; i++){
+        var finalPrice;
+        if(taxBoolean == true){
+            finalPrice = dishData[i].price + dishData[i].price * 0.2; 
+        }
+        else if(taxBoolean == false){
+            finalPrice = dishData[i].price;
+        }
+        else{
+            console.log("You need to pass a boolean to the getPrices call!")
+            return;
+        }
+        console.log("Dish: " +dishData[i].name+ " Price: $" + finalPrice);
+    }
 }
 
 // Implement getDiscount()
-function getDiscount() {
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
     
+    if((guests > 0 && guests < 30)
+    && typeof guests === 'number'){
+        var discount = 0;
+        if(guests < 5){discount = 5;}
+        else if(guests >= 5){discount = 10;}
+        console.log("Discount is: $" + discount);
+    }
+
+    else {
+        console.log("The second argument must be a number between 0 and 30")
+    } 
 }
 
 // Call getDiscount()
+getDiscount(true, 4);
+getDiscount(false, 10);
